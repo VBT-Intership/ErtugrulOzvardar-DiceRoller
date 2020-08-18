0319b5436b96d569fcb8bcaa5e3dfd4598952eba
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-08-18 19:37:53 +0300

    TextView attribute inside the xml file completely dropped. Instead, An ImageView attribute which can stand for real dice images is added. Then, rollDice function changed accordingly for our dice images. Now, whenever the button is clicked, random dice images with their random values will appear on the screen. (Initially set to 1 as dice_1 in xml file)

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,8 +24,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice: TextView = findViewById(R.id.result_text)
+
         val randomValue = Random.nextInt(1,7)
-        dice.text = randomValue.toString()
+        val diceImg: ImageView = findViewById(R.id.dice_image)
+            val drawableResource = when (randomValue){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        diceImg.setImageResource(drawableResource)
     }
 }

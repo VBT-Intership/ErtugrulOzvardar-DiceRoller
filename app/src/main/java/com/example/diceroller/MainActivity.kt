bdf11bb34a9e0c681a059fd894e1549f9c473bcf
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-08-18 19:00:08 +0300

    an id has been added for the Button as roll_button. And, this Button is created inside the MainActivity class and derived from the findViewById method. Also, our button text is changed directly from the MainActivity class.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.diceroller
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll!"
     }
 }
